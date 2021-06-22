39e06d949a79999904d5cd4c5ace1bcb79310964
Stefano Mezzini <stefano@menouno.com>
2021-06-22 10:25:16 -0600

    improved meta figure, need to add some means

--- analysis/tapir-models.R
+++ analysis/tapir-models.R
@@ -3,10 +3,16 @@ library('readxl')  # for read_xlsx()
 library('ctmm')    # using the github version (0.6.1)
 library('dplyr')   # for data wrangling
 library('purrr')   # for vectorization
+library('furrr')   # for parallel vectorization
 library('tidyr')   # for data wrangling
 library('ggplot2') # for plotting
-theme_set(theme_bw())
+theme_set(theme_bw() +
+            theme(panel.grid = element_blank()))
 N <- 74 # number of tapirs
+
+# color palette
+pal <- c('#4477AA', '#ff8c00', '#66CCEE', '#009900',
+         '#CCBB44', '#EE6677', '#AA3377', '#BBBBBB')
 
 # for local parallel computation (windows)
 library('furrr') # for parallel computation
@@ -120,7 +126,7 @@ if(FALSE) {
   # In ctmm.fit(data, GUESS, trace = trace2, ...) :
   # pREML failure: indefinite ML Hessian or divergent REML gradient.
 } else {
-  tapirs <- readRDS('models/tapirs-calibrated.rds')
+  tapirs <- readRDS('models/tapirs-akdes.rds')
 }
 
 # plot of times
@@ -184,13 +190,6 @@ save.plots <- function(i) {
 future_map(1:N, save.plots, .options = furrr_options(seed = NULL))
 dev.off() # make sure all pdf devices are closed
 
-# estimate speed and home range
-tapirs <- mutate(tapirs,
-                 # missing speeds are returned as m/s while others as km/day
-                 speed.est = future_map(model, function(x) list(speed(x)),
-                                        .options = furrr_options(seed = NULL)))
-unique(warnings()) # 21 have fractal movement
-
 # withTimeout() cannot be used with map()
 tictoc::tic() # ~ 3 minutes without weights, ~ 25 minutes with weights
 for(i in 1:N) {
@@ -219,20 +218,50 @@ plot.estimates <- function(ROW) {
 }
 
 # extract estimates
-tapirs <-
-  bind_cols(tapirs,
-            bind_rows(map(tapirs$model, # autocorrelation parameters
-                          function(x) {
-                            y <- c(tau = x$tau)
-                            if(is.null(y)) y <- c(tau.position = NA,
-                                                  tau.velocity = NA)
-                            y
-                          })),
-            bind_rows(map(tapirs$speed.est, as.data.frame)), # speed estimates
-            map(1:N, function(i) tapirs$akde[[i]]$DOF.area %>% # akde df
+foo <- function(x) {
+  cnames <- c('speed.est', 'speed.high', 'speed.low', 'tau.position.est', 'tau.position.high', 'tau.position.low',
+              'tau.velocity.est', 'tau.velocity.high', 'tau.velocity.low')
+  
+  # extract estimates and CIs from summaries
+  SUM <- summary(x, units = FALSE)$CI %>% as.data.frame()
+  SUM <- mutate(SUM,
+                param = rownames(SUM),
+                param = substr(param, 2, 3)) %>% # tau causes issues
+    dplyr::filter(!(grepl('r', param))) %>%
+    mutate(param = case_when(param == '[p' ~ 'tau.position',
+                             param == '[v' ~ 'tau.velocity',
+                             param == 'pe' ~ 'speed'),
+           param.h = paste0(param, '.high'),
+           param.l = paste0(param, '.low'),
+           param = paste0(param, '.est')) %>%
+    pivot_wider(names_from = param.l, values_from = low) %>%
+    pivot_wider(names_from = param, values_from = est) %>%
+    pivot_wider(names_from = param.h, values_from = high) %>%
+    colSums(na.rm = TRUE) %>%
+    t() %>%
+    as_tibble()
+  NAs <- cnames[! cnames %in% colnames(SUM)]
+  NAs
+  if(length(NAs) > 0){
+    SUM <- SUM %>%
+      bind_cols(rep(NA_real_, length(NAs)) %>%
                   t() %>%
-                  as.data.frame()) %>%
-              bind_rows() %>%
+                  matrix(dimnames = list(NULL, NAs), nrow = 1) %>%
+                  as_tibble())
+  }
+  SUM[cnames]
+}
+foo(tapirs$model[[1]])
+tapirs <-
+  select(tapirs, -speed.est, -tau.position, -tau.velocity, -x.df, -y.df) %>%
+  bind_cols(map_dfr(tapirs$model, .f = foo) %>% # tau and speed parameters
+              mutate(speed.low = speed.low * 86.4,
+                     speed.est = speed.est * 86.4,
+                     speed.high = speed.high * 86.4),
+            map_dfr(1:N,
+                    function(i) tapirs$akde[[i]]$DOF.area %>% # akde df
+                      t() %>%
+                      as.data.frame()) %>%
               rename(x.df = x, y.df = y))
 
 # AKDE plots with data, open plots with SumatraPDF
@@ -247,7 +276,7 @@ tapirs <- mutate(tapirs,
 # speed estimates (only one speed estimate in mata atlantica)
 s.box <-
   tapirs %>%
-  select(region, name, low, est, high, age, sex, adult) %>%
+  dplyr::select(region, name, low, est, high, age, sex, adult) %>%
   pivot_longer(low:high, names_to = 'parameter') %>%
   mutate(parameter = factor(parameter, levels = c('low', 'est', 'high')))%>%
   ggplot() +
@@ -267,7 +296,7 @@ ggsave('figures/speed-boxplots-age.png',
 
 # tau estimates (only one speed estimate in mata atlantica)
 t.box <- tapirs %>%
-  select(region, name, tau.position, tau.velocity, sex, adult) %>%
+  dplyr::select(region, name, tau.position, tau.velocity, sex, adult) %>%
   pivot_longer(tau.position:tau.velocity, names_to = 'parameter') %>%
   mutate(parameter = case_when(parameter == 'tau.position' ~ 'Position',
                                parameter == 'tau.velocity' ~ 'Velocity')) %>%
@@ -317,15 +346,125 @@ pdf('figures/akde/all-cerrado.pdf', width = 8, height = 8)
 plot(tapirs$akde[58:74])
 dev.off()
 
-tapirs <- mutate(tapirs,
-                 color = case_when(region == 'atlantica' ~ '#ff8c00',
-                                   region == 'pantanal' ~ '#4477AA',
-                                   region == 'cerrado' ~ '#009900'))
-png('figures/meta-models.png', width = 8, height = 8, units = 'in', res = 300)
-meta(tapirs$model, col = tapirs$color)
-dev.off()
-png('figures/meta-akdes.png', width = 8, height = 8, units = 'in', res = 300)
-meta(tapirs$akde, col = tapirs$color)
-dev.off()
+tapirs <-
+  mutate(tapirs,
+         region.lab = case_when(region == 'atlantica' ~ 'Mata Atlantica',
+                                region == 'pantanal' ~ 'Pantanal',
+                                region == 'cerrado' ~ 'Cerrado'),
+         # use units = FALSE to force SI units
+         bind_cols(map_dfr(tapirs$akde,
+                           function(x) summary(x, units = FALSE)$CI %>%
+                             as_tibble()) %>%
+                     transmute(area.low = low / 1e6,
+                               area.est = est / 1e6,
+                               area.high = high / 1e6))) # convert m^2 to km^2
 
-saveRDS(tapirs, 'models/tapirs-akdes.rds')
+# Figure 2 ####
+tap <-
+  bind_rows(
+    tapirs,
+    bind_cols(
+      name = c('Mata Atlantica', 'Pantanal', 'Cerrado', 'Overall'),
+      region.lab = c('Mata Atlantica', 'Pantanal', 'Cerrado', 'Overall'),
+      bind_rows(meta(tapirs$akde[tapirs$region == 'atlantica'],plot=FALSE)[1, ],
+                meta(tapirs$akde[tapirs$region == 'pantanal'], plot=FALSE)[1, ],
+                meta(tapirs$akde[tapirs$region == 'cerrado'], plot=FALSE)[1, ],
+                meta(tapirs$akde, plot = FALSE)[1, ])) %>%
+      rename(area.low = low, area.est = est, area.high = high)) %>%
+  
+  # add average s for speeds and tau parameters ################################
+  
+  mutate(name = factor(name,
+                       levels = c(unique(tapirs$name), 'Mata Atlantica',
+                                  'Pantanal', 'Cerrado', 'Overall')),
+         region.lab = factor(region.lab,
+                             levels = c('Mata Atlantica', 'Pantanal', 'Cerrado',
+                                        'Overall')),
+         average = name %in% c('Mata Atlantica', 'Pantanal', 'Cerrado', 'Overall'))
+
+# 2a) meta() of areas
+p.areas <-
+  ggplot(tap) +
+  geom_segment(aes(x = area.low, xend = area.high, y = name, yend = name,
+                   color = region.lab), lwd = 2) +
+  geom_point(aes(x = area.est, y = name, shape = average), col = 'black', size = 1.5) +
+  geom_point(aes(x = area.est, y = name, shape = average), col = 'white', size = 1) +
+  scale_shape_manual(values = c(19, 17)) +
+  scale_color_manual(values = c(pal[1:3], 'black')) +
+  scale_y_discrete(limits = rev,
+                   labels = c('Means', 'Cerrado', 'Pantanal', 'Mata Atlantica'),
+                   breaks = c('Mata Atlantica', 'CE_15_KURUKA',
+                              'PA_33_GABRIELA', 'AF_14_JAMESBOND')) +
+  theme(legend.position = 'none', axis.ticks.y = element_blank()) +
+  labs(x = bquote('Estimated 95% home range area'~(km^2)),
+       y = NULL); p.areas
+
+# 2b) home range crossing times
+p.tau.pos <-
+  ggplot(tap) +
+  geom_segment(aes(x = tau.position.low / (60^2 * 24),
+                   xend = tau.position.high / (60^2 * 24), y = name, yend = name,
+                   color = region.lab), lwd = 2) +
+  geom_point(aes(x = tau.position.est / (60^2 * 24), y = name, shape = average),
+             col = 'black', size = 1.5) +
+  geom_point(aes(x = tau.position.est / (60^2 * 24), y = name, shape = average),
+             col = 'white', size = 1) +
+  scale_shape_manual(values = c(19, 17)) +
+  scale_color_manual(values = c(pal[1:3], 'black')) +
+  scale_y_discrete(limits = rev,
+                   labels = c('Means', 'Cerrado', 'Pantanal', 'Mata Atlantica'),
+                   breaks = c('Mata Atlantica', 'CE_15_KURUKA',
+                              'PA_33_GABRIELA', 'AF_14_JAMESBOND')) +
+  theme(legend.position = 'none', axis.ticks.y = element_blank()) +
+  labs(x = 'Range crossing time (days)', y = NULL); p.tau.pos
+
+# 2c) velocity autocorrelation timescale
+p.tau.vel <-
+  ggplot(tap) +
+  geom_segment(aes(x = tau.velocity.low / 60^2,
+                   xend = tau.velocity.high / 60^2, y = name, yend = name,
+                   color = region.lab), lwd = 2) +
+  geom_point(aes(x = tau.velocity.est / 60^2, y = name, shape = average),
+             col = 'black', size = 1.5) +
+  geom_point(aes(x = tau.velocity.est / 60^2, y = name, shape = average),
+             col = 'white', size = 1) +
+  scale_shape_manual(values = c(19, 17)) +
+  scale_color_manual(values = c(pal[1:3], 'black')) +
+  scale_y_discrete(limits = rev,
+                   labels = c('Means', 'Cerrado', 'Pantanal', 'Mata Atlantica'),
+                   breaks = c('Mata Atlantica', 'CE_15_KURUKA',
+                              'PA_33_GABRIELA', 'AF_14_JAMESBOND')) +
+  theme(legend.position = 'none', axis.ticks.y = element_blank()) +
+  labs(x = 'Directional persistence timescale (hours)', y = NULL); p.tau.vel
+
+# 2d) mean movement speeds
+p.speeds <-
+  ggplot(tap) +
+  geom_segment(aes(x = speed.low, xend = speed.high,
+                   y = name, yend = name, color = region.lab), lwd = 2,
+               show.legend = FALSE) +
+  geom_point(aes(x = speed.est, y = name, shape = average), col = 'black',
+             size = 1.5, show.legend = FALSE) +
+  geom_point(aes(x = speed.est, y = name, shape = average), col = 'white',
+             size = 1, show.legend = FALSE) +
+  scale_shape_manual(values = c(19, 17)) +
+  scale_color_manual(values = c(pal[1:3], 'black')) +
+  scale_y_discrete(limits = rev,
+                   labels = c('Mata Atlantica', 'Pantanal', 'Cerrado', 'Means'),
+                   breaks = c('AF_17_ESPERTA', 'PA_33_GABRIELA', 'CE_15_KURUKA',
+                              'Mata Atlantica')) +
+  theme(legend.position = 'none',
+        panel.grid.major.y = element_blank(),
+        axis.ticks.y = element_blank()) +
+  labs(x = 'Estimated average speed (km/day)', y = NULL); p.speeds
+
+cowplot::plot_grid(p.areas,
+                   p.tau.pos,
+                   p.tau.vel,
+                   p.speeds,
+                   labels = c('a.', 'b.', 'c.', 'd.'),
+                   label_fontface = 'plain',
+                   ncol = 2, byrow = TRUE)
+
+ggsave('figures/meta.png', width = 10, height = 8)
+#saveRDS(tapirs, 'models/tapirs-final.rds')
