afecc8fa9d4e3b652e541dc16ade6f4f87332d03
Stefano Mezzini <stefano@menouno.com>
2021-07-29 10:42:08 -0600

    added land use regressions on tau_v

--- analysis/land-use-regressions.R
+++ analysis/land-use-regressions.R
@@ -8,6 +8,7 @@ library('purrr')      # for functional mapping
 library('ggplot2')    # for plotting
 library('cowplot')    # for plot grids
 library('mgcv')       # for fitting GAMs
+library('gratia')     # for GAM plots
 theme_set(theme_bw())
 N <- 74 # number of tapirs
 
@@ -15,18 +16,18 @@ N <- 74 # number of tapirs
 pal <- c('#4477AA', '#ff8c00', '#66CCEE', '#009900',
          '#CCBB44', '#EE6677', '#AA3377', '#BBBBBB')
 
-sa <- filter(spData::world, continent == 'South America') # south america layer
-
-# land use rasters
-lu.atl <- raster('data/spatial-layers/1- ATLANTIC FOREST/MA_Habitat.tif')
-lu.pan <-raster('data/spatial-layers/2- PANTANAL/PANTANAL_USO_SOLO_03_2017.tif')
-lu.cer <- raster('data/spatial-layers/3- CERRADO/CERRADO_USO_SOLO_09_2016.tif')
-
 # extract land use raster and land use types
-tapirs <- readRDS('models/tapirs-land-use.rds') # tapir data
+tapirs <- readRDS('models/tapirs-land-use.rds') %>% # tapir data
+  mutate(tau.velocity.est = tau.velocity.est / 60 / 60) # from seconds to hours
 
 if(FALSE) { # re-crop rasters
-  tapirs <- readRDS('models/tapirs-final.rds') # tapir data
+  # land use rasters
+  lu.atl <- raster('data/spatial-layers/1- ATLANTIC FOREST/MA_Habitat.tif')
+  lu.pan <-raster('data/spatial-layers/2- PANTANAL/PANTANAL_USO_SOLO_03_2017.tif')
+  lu.cer <- raster('data/spatial-layers/3- CERRADO/CERRADO_USO_SOLO_09_2016.tif')
+  
+  # tapir data without clipped rasters
+  tapirs <- readRDS('models/tapirs-final.rds')
   
   tapirs <-
     mutate(tapirs,
@@ -115,7 +116,6 @@ if(FALSE) { # re-crop rasters
 # groups with less than 5 unique values
 select(tapirs, `?`:plantation) %>%
   pivot_longer(-c()) %>%
-  filter(value > 0) %>%
   group_by(name) %>%
   summarize(unique = length(unique(value)),
             min = min(value),
@@ -138,30 +138,100 @@ m.hr <- gam(area.est ~ s(dirt, k = 5),
             method = 'REML')
 summary(m.hr)
 
-# Gamma GAM regression on home range estimate
+# Gamma GAM regression on average speed estimate
 m.speed <- gam(speed.est ~
-                 s(floodplain, k = 5) +
-                 s(dirt, k = 5) +
+                 s(forest, k = 10) +
+                 s(floodplain, k = 10) +
+                 s(savannah, k = 10) +
+                 s(dirt, k = 10) +
                  s(pasture, k = 5) +
-                 s(savannah, k = 5) +
-                 s(forest, k = 5) +
-                 s(headquarters, k = 5) +
                  s(crop, k = 5) +
                  s(water, k = 5),
                family = Gamma('log'),
                data = tapirs,
                method = 'REML')
-m.speed0 <- gam(speed.est ~ 1,
-               family = Gamma('log'),
-               data = tapirs,
-               method = 'REML')
-AIC(m.speed, m.speed0)
+m.speed0 <- gam(speed.est ~ forest,
+                family = Gamma('log'),
+                data = tapirs,
+                method = 'REML')
+AIC(m.speed, m.speed0) # use null model
+
+# Gamma GAM regression on average tau_v estimate
+filter(tapirs, !is.na(tau.velocity.est)) %>%
+  select(`?`:plantation) %>%
+  pivot_longer(-c()) %>%
+  group_by(name) %>%
+  summarize(unique = length(unique(value)),
+            min = min(value),
+            max = max(value)) %>%
+  arrange(desc(unique)) %>%
+  mutate(`>5` = unique > 5,
+         `>10` = unique > 10) %>%
+  filter(`>5`)
+
+m.tauv <- gam(tau.velocity.est ~
+                s(forest, k = 10) +
+                s(floodplain, k = 10) +
+                s(savannah, k = 10) +
+                s(dirt, k = 5) +
+                s(pasture, k = 5) +
+                s(crop, k = 5) +
+                s(water, k = 5),
+              family = Gamma('log'),
+              data = tapirs,
+              method = 'REML')
+summary(m.tauv)
+AIC(m.tauv)
+m.tauv <- gam(tau.velocity.est ~
+                s(forest, k = 10) +
+                # s(floodplain, k = 10) +
+                # s(savannah, k = 10) +
+                # s(dirt, k = 5) +
+                # s(pasture, k = 5) +
+                # s(crop, k = 5) +
+                s(water, k = 5),
+              family = Gamma('log'),
+              data = tapirs,
+              method = 'REML')
+AIC(m.tauv)
+summary(m.tauv)
+
+# Gamma GAM regression on average tau_v estimate without outlier
+m.tauv.1 <- gam(tau.velocity.est ~
+                  s(forest, k = 10) +
+                  s(floodplain, k = 10) +
+                  s(savannah, k = 10) +
+                  s(dirt, k = 5) +
+                  s(pasture, k = 5) +
+                  s(crop, k = 5) +
+                  s(water, k = 5),
+                family = Gamma('log'),
+                data = filter(tapirs, tau.velocity.est < 1.5),
+                method = 'REML')
+summary(m.tauv.1)
+AIC(m.tauv.1) # 647.7876
+m.tauv.1 <- gam(tau.velocity.est ~
+                  s(forest, k = 10) +
+                  # s(floodplain, k = 10) +
+                  # s(savannah, k = 10) +
+                  # s(dirt, k = 5) +
+                  # s(pasture, k = 5) +
+                  # s(crop, k = 5) +
+                  s(water, k = 5),
+                family = Gamma('log'),
+                data = filter(tapirs, tau.velocity.est < 1.5),
+                method = 'REML')
+AIC(m.tauv.1)
+summary(m.tauv.1)
 
 # initial plots
-gratia::draw(m.hr, residuals = TRUE)
-gratia::draw(m.speed, residuals = TRUE)
+draw(m.hr, residuals = TRUE)
+draw(m.speed, residuals = TRUE)
+plot_grid(draw(m.tauv, residuals = TRUE), draw(m.tauv.1, residuals = TRUE),
+          ncol = 1)
 
 # predictions ####
+# lu on hr
 pred <- tibble(dirt = seq(0, 0.22, length.out = 100))
 pred <- bind_cols(pred,
                   predict(m.hr, newdata = pred, se.fit = TRUE)) %>%
@@ -180,4 +250,52 @@ p <-
        y = expression('Home Range Area'~(km^2))) +
   theme(legend.position = 'top'); p
 
-ggsave('figures/lu-regression.png', height = 1.5, width = 3.23, scale = 2)
+# ggsave('figures/lu-regression-hr.png', height = 1.5, width = 3.23, scale = 2)
+
+# lu on tau_v
+pred1 <- tibble(forest = seq_min_max(tapirs$forest, n = 100),
+                water = mean(tapirs$water, na.rm = TRUE))
+pred1 <-
+  bind_cols(pred1, predict(m.tauv.1, newdata = pred1, se.fit = TRUE,
+                           terms = 's(forest)')) %>%
+  mutate(est = exp(fit),
+         lwr = exp(fit - 1.96 * se.fit),
+         upr = exp(fit + 1.96 * se.fit))
+
+pred2 <- tibble(forest = mean(tapirs$forest, na.rm = TRUE),
+                water = seq_min_max(tapirs$water, n = 100))
+pred2 <-
+  bind_cols(pred2, predict(m.tauv.1, newdata = pred2, se.fit = TRUE,
+                           terms = 's(water)')) %>%
+  mutate(est = exp(fit),
+         lwr = exp(fit - 1.96 * se.fit),
+         upr = exp(fit + 1.96 * se.fit))
+
+# regression plot
+p <-
+  plot_grid(
+    ggplot() +
+      geom_ribbon(aes(forest, ymin = lwr, ymax = upr), pred1, alpha = 0.2) +
+      geom_line(aes(forest, est), pred1) +
+      geom_point(aes(forest, tau.velocity.est, color = region.lab,
+                     shape = tau.velocity.est > 1.5),
+                 filter(tapirs, ! is.na(tau.velocity.est)), alpha = 0.9) +
+      scale_shape_manual('Outlier', values = c(19, 4), labels = c('No', 'Yes')) +
+      scale_color_manual('Region', values = pal[1:3]) +
+      labs(x = 'Proportion of forested habitat',
+           y = 'Directional persistence (hours)') +
+      theme(legend.position = 'top'),
+    ggplot() +
+      geom_ribbon(aes(water, ymin = lwr, ymax = upr), pred2, alpha = 0.2) +
+      geom_line(aes(water, est), pred2) +
+      geom_point(aes(water, tau.velocity.est, color = region.lab,
+                     shape = tau.velocity.est > 1.5),
+                 filter(tapirs, ! is.na(tau.velocity.est)), alpha = 0.9) +
+      scale_shape_manual('Outlier', values = c(19, 4), labels = c('No', 'Yes')) +
+      scale_color_manual('Region', values = pal[1:3]) +
+      labs(x = 'Proportion of water in the habitat',
+           y = 'Directional persistence (hours)') +
+      theme(legend.position = 'none'),
+    ncol = 1, rel_heights = c(1.2, 1)); p
+
+# ggsave('figures/lu-regression-tau-v.png', height = 3, width = 3.23, scale = 2)
